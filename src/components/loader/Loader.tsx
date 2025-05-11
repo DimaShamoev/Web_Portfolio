d83f2b72512a498a543a3da86db33363fb64fa16
Dima Shamoev <dimashamoev5@gmail.com>
2025-05-11 22:03:47 +0400

    create custom loader

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,14 @@
+const Loader = () => {
+    return (
+        <div className="bg-[#1A191D] h-screen flex flex-col items-center justify-center fill-mask gap-4 absolute inset-0 box-padding">
+            <div className="loader-text text-2xl font-bold mix-blend-screen bg-slate-800 flex items-center justify-center w-full max-w-[450px] sm:text-5xl">
+                Dima Shamoev
+            </div>
+            <div className="loading-bar w-full max-w-[450px] h-2 bg-gray-500">
+                <div className="progress-bar h-full bg-white" />
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
