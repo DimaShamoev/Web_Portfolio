448a4f2dabcdc616532ed58d18f4c376d913154f
Dima Shamoev <dimashamoev5@gmail.com>
2025-05-11 15:24:23 +0400

    cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Header from "./components/Header"
-import ParticlesBackground from "./components/ParticlesBackground"
 
 const App: React.FunctionComponent = () => {
     return (
         <div className="App">
             <div className="container">
                 <Header />
-                <ParticlesBackground />
             </div>
         </div>
     )
