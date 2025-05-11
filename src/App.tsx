366b4dafcfa8f9814a4b1fbfc0fe964f6db4cff8
Dima Shamoev <dimashamoev5@gmail.com>
2025-05-11 18:50:36 +0400

    cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Header from "./components/Header"
-
 const App: React.FunctionComponent = () => {
     return (
         <div className="App">
             <div className="container">
-                <Header />
+                
             </div>
         </div>
     )
